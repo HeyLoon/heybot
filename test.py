bff72acb8255751a4a5708b60242bf4771176f7f
HeyLoon <heyloonli@gmail.com>
2023-08-12 21:27:46 +0800

    Add new test

--- test.py
+++ test.py
@@ -11,3 +11,8 @@ response = requests.get(w_url, params=params)
 data = response.json()
 
 print(data['images'][0]['url'],data['images'][0]['artist']['pixiv'])
+
+c_url = 'https://www.twtainan.net/data/attractions_zh-tw.json'
+response = requests.get(c_url)
+data = response.json()
+print(data['name'])
